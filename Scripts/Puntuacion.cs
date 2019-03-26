48513f6c9a5dd240de9ebd79a7278e30f2c0546d
Trope100 <48563021+Trope100@users.noreply.github.com>
2019-03-26 18:56:01 +0100

    Add files via upload

---
+++ Scripts/Puntuacion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puntuacion: MonoBehaviour{
+
+    GameManager gm;
+
+     void Awake(){
+
+        gm = FindObjectOfType<GameManager>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+       if (other.gameObject.tag == "Moneda"){
+            gm.ActualizarMonedas();
+            gm.ActualizarPuntos(100);
+            Destroy(other.gameObject);
+
+        }
+        if (other.gameObject.tag == "Cereza") {
+            gm.ActualizarVidas();
+            gm.ActualizarPuntos(300);
+            Destroy(other.gameObject);
+
+        }
+
+        if(other.gameObject.tag == "Rubi") {
+            Debug.Log("toca");
+            gm.ActualizarRubi();
+            gm.ActualizarPuntos(500);
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+
+
+
+
+}
